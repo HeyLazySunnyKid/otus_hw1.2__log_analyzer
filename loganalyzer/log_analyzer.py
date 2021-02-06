b64b2cd7a32adaa8a32169f8a633d99f9676b111
Denis Kalyuzhnyy <denis.kaluzhnyy@gmail.com>
2021-02-06 09:44:52 +0300

    [+] get_latest_logfile with code and tests

--- loganalyzer/log_analyzer.py
+++ loganalyzer/log_analyzer.py
@@ -9,21 +9,27 @@
 
 import logging
 import sys
-from collections import namedtuple
+import re
 from os import listdir
 from os.path import isfile, join
-from typing import NamedTuple
+from typing import Dict, Optional, Tuple, NamedTuple, Union
 
-Lograw = NamedTuple('Lograw', [
-    ['url', str],           # Url запроса
-    ['count', int],         # сколько раз встречается URL, абсолютное значение
-    ['count_perc', float],  # сколько раз встречается URL, в процентнах относительно общего числа запросов
-    ['time_avg', float],    # средний $request_time для данного URL'а
-    ['time_max', float],    # максимальный $request_time для данного URL'а
-    ['time_med', float],    # медиана $request_time для данного URL'а
-    ['time_perc', float],   # сколько раз встречается URL, в процентнах относительно общего числа запросов
-    ['time_sum', float]     # суммарный $request_time для данного URL'а, абсолютное значение
-])
+
+class Lograw(NamedTuple):
+    url: str           # Url запроса
+    count_abs: int     # сколько раз встречается URL, абсолютное значение
+    count_perc: float  # сколько раз встречается URL, в процентнах относительно общего числа запросов
+    time_avg: float    # средний $request_time для данного URL'а
+    time_max: float    # максимальный $request_time для данного URL'а
+    time_med: float    # медиана $request_time для данного URL'а
+    time_perc: float   # сколько раз встречается URL, в процентнах относительно общего числа запросов
+    time_sum: float    # суммарный $request_time для данного URL'а, абсолютное значение
+
+class Filestat(NamedTuple):
+    path: str
+    data: str
+    extention: str
+
 
 config = {
     "REPORT_SIZE": 1000,
@@ -31,17 +37,37 @@ config = {
     "LOG_DIR": "./log"
 }
 
-def get_latest_logfile(logdir: str) -> (str, str):
-    """TODO: Docstring for get_latest_logfile.
 
-    :logdir: TODO
-    :returns: TODO
+def get_latest_logfile(logdir: str) -> Optional[Filestat]:
+    """ Return info about latest log inside logdir
+
+    :logdir: path to logdir
+    :returns: None if logfile not found or Filestat about latest file
 
     """
-    pass
+
+    # TODO: move file start to config
+    parser = re.compile(r'nginx-access-ui.log-(\d+).?(\w*)')
+    last = None
+    for file in listdir(logdir):
+        match = parser.match(file)
+        if match is None:
+            continue
+
+        data = match.group(1)
+        if match.group(2) in ['', 'log', 'txt']:
+            extention = 'plain'
+        elif match.group(2) in ['gz', 'gzip']:
+            extention = 'gzip'
+        else:
+            continue
+        if last is None or data > last.data:
+            last = Filestat(join(logdir, match.group(0)),
+                            data, extention)
+    return last
 
 
-def parse_logfile(log) -> List[Lograw]:
+def parse_logfile(logfile: str) -> Tuple[Lograw]:
     """TODO: Docstring for parse_logfile.
 
     :arg1: TODO
@@ -51,7 +77,7 @@ def parse_logfile(log) -> List[Lograw]:
     pass
 
 
-def load_config(configfile=None: str) -> dict:
+def load_config(configfile: Optional[str] = None) -> Dict[str, Union[str, int]]:
     """TODO: Load configuration and validate it
 
     :configfile: Configuration file (if defined)
@@ -60,8 +86,10 @@ def load_config(configfile=None: str) -> dict:
     """
     pass
 
-def main():
+
+def main() -> None:
     pass
+
 
 if __name__ == "__main__":
     main()
---
+++ tests/tests_log_analyzer.py
@@ -0,0 +1,41 @@
+import unittest
+from unittest.mock import patch
+import loganalyzer.log_analyzer as la
+
+
+class TestLogAnylyzer(unittest.TestCase):
+
+    def setUp(self):
+        self.general_filenames = [
+            'nginx-access-ui.log-20190629.gz',
+            'nginx-access-ui.log-20190630.gz',
+            'nginx-access-ui.log-20190629',
+            'apache-access-ui.log-20190631.gz'
+        ]
+
+    def test_get_latets_logfile_gzip(self):
+        with patch('loganalyzer.log_analyzer.listdir',
+                   return_value=self.general_filenames):
+            path, data, extention = la.get_latest_logfile('./test_directory')
+
+        self.assertEqual(path, './test_directory/nginx-access-ui.log-20190630.gz')
+        self.assertEqual(data, '20190630')
+        self.assertEqual(extention, 'gzip')
+
+    def test_get_latest_logfile_plain(self):
+        self.general_filenames.append('nginx-access-ui.log-20190631')
+        with patch('loganalyzer.log_analyzer.listdir',
+                   return_value=self.general_filenames):
+            path, data, extention = la.get_latest_logfile('./test_directory')
+        self.assertEqual(path, './test_directory/nginx-access-ui.log-20190631')
+        self.assertEqual(data, '20190631')
+        self.assertEqual(extention, 'plain')
+
+    def test_get_latest_logfile_none(self):
+        filesnames = [
+            'apache-access-ui.log-20190631.gz',
+            'postgresql-access-ui.log-20190631'
+        ]
+        with patch('loganalyzer.log_analyzer.listdir',
+                    return_value=[]):
+            self.assertIsNone(la.get_latest_logfile('./test_directory'))
